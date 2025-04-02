285c2ae2f6c16b5d0adc35fb0accaccab74a61ff
Will Thornton <127424455+oldballs81@users.noreply.github.com>
2025-04-01 22:07:11 -0300

    updated without stashed
    final commit

--- thread_rcvdata.py
+++ thread_rcvdata.py
@@ -11,16 +11,7 @@ import pandas as pd
 import math
 import time
 import multiprocessing
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 import queue
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 #from common import data
 
 #### CLASSES ####
@@ -30,16 +21,7 @@ class Worker_DAQ(QObject):
     def __init__(self, q_data):
         super(Worker_DAQ, self).__init__()
         self.data_queue = q_data
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
         self.running = True
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
         #self.data = data
         
     #    def run(self):
@@ -52,9 +34,6 @@ class Worker_DAQ(QObject):
     num_samples_window = 50
     
     def run(self):
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
         while self.running:
             try:
                 #pop queue and pass it through signal to front end
@@ -62,27 +41,6 @@ class Worker_DAQ(QObject):
                 self.sendData.emit(batch)
             except queue.Empty:
                 continue 
-=======
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-        while(True):
-            #waiting until there is seomthing in queue
-            while self.data_queue.empty():
-                pass 
-            
-            #pop queue and pass it through signal to front end
-            sample = self.data_queue.get()
-            self.sendData.emit(sample) 
-            
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
             
     def test_run(self):
         count = 1
